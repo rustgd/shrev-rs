27fae98e0135ce6c1b7c4163d953d6663768ebca
Simon Rönnberg <simon.ronnberg@codemill.se>
2017-07-28 14:25:41 +0200

    REFACTOR: Moving read indices into the reader id, to make read operations immutable. REFACTOR: Moving the write index and max size into the ring buffer storage to remove the need for hashmap lookups

--- examples/basic.rs
+++ examples/basic.rs
@@ -26,13 +26,13 @@ fn main() {
     let mut events = vec![TestEvent2 { data : 3}, TestEvent2 { data : 4}];
     event_handler.write(&mut events);
 
-    let reader_id_2 = event_handler.register_reader::<TestEvent2>();
+    let mut reader_id_2 = event_handler.register_reader::<TestEvent2>();
 
-    println!("{:?}", event_handler.read::<TestEvent2>(reader_id_2));
-    println!("{:?}", event_handler.read::<TestEvent2>(reader_id_2));
+    println!("{:?}", event_handler.read::<TestEvent2>(&mut reader_id_2));
+    println!("{:?}", event_handler.read::<TestEvent2>(&mut reader_id_2));
 
     let mut events = vec![TestEvent2 { data : 8}, TestEvent2 { data : 9}];
     event_handler.write(&mut events);
-    println!("{:?}", event_handler.read::<TestEvent2>(reader_id_2));
+    println!("{:?}", event_handler.read::<TestEvent2>(&mut reader_id_2));
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,28 @@ extern crate shred;
 
 use std::ops::{Index, IndexMut};
 use std::any::TypeId;
-use std::collections::HashMap;
 
 const MAX_EVENTS: usize = 200;
 
-struct VecStorage<T> {
-    data : Vec<T>
+struct RingBufferStorage<T> {
+    data : Vec<T>,
+    write_index : usize,
+    max_size : usize
 }
 
 pub trait Event : Send + Sync + Clone + 'static {}
 
-impl<T: Clone> VecStorage<T> {
+impl<T: Clone> RingBufferStorage<T> {
     pub fn new(size : usize) -> Self {
-        VecStorage { data: Vec::with_capacity(size) }
+        RingBufferStorage {
+            data: Vec::with_capacity(size),
+            write_index : 0,
+            max_size : size
+        }
     }
 }
 
-impl<T> Index<usize> for VecStorage<T> {
+impl<T> Index<usize> for RingBufferStorage<T> {
     type Output = T;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -26,14 +31,28 @@ impl<T> Index<usize> for VecStorage<T> {
     }
 }
 
-impl<T> IndexMut<usize> for VecStorage<T> {
+impl<T> IndexMut<usize> for RingBufferStorage<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.data[index]
     }
 }
 
 #[derive(Hash, Eq, PartialEq, Copy, Clone)]
-pub struct ReaderId(TypeId, u32);
+pub struct ReaderId {
+    t : TypeId,
+    id : u32,
+    read_index : usize
+}
+
+impl ReaderId {
+    pub fn new(t : TypeId, id : u32) -> ReaderId {
+        ReaderId {
+            t : t,
+            id : id,
+            read_index : 0,
+        }
+    }
+}
 
 pub struct EventHandler {
     res : shred::Resources,
@@ -42,33 +61,29 @@ pub struct EventHandler {
 
 impl EventHandler {
     pub fn new() -> EventHandler {
-        let mut handler = EventHandler {
+        EventHandler {
             res : shred::Resources::new(),
             next_reader_id: 0
-        };
-        handler.res.add(HashMap::<ReaderId, usize>::default());
-        handler.res.add(HashMap::<TypeId, usize>::default());
-        handler
+        }
     }
 
     pub fn register<E : Event>(&mut self) {
+        self.register_with_size::<E>(MAX_EVENTS);
+    }
+
+    pub fn register_with_size<E : Event>(&mut self, max_size : usize) {
         use shred::ResourceId;
 
-        if self.res.has_value(ResourceId::new::<VecStorage<E>>()) {
+        if self.res.has_value(ResourceId::new::<RingBufferStorage<E>>()) {
             return;
         }
 
-        self.res.add(VecStorage::<E>::new(MAX_EVENTS));
-
-        let mut writers = self.res.fetch_mut::<HashMap<TypeId, usize>>(0);
-        writers.insert(TypeId::of::<E>(), 0);
+        self.res.add(RingBufferStorage::<E>::new(max_size));
     }
 
     pub fn register_reader<E : Event>(&mut self) -> ReaderId {
-        let reader_id = ReaderId(TypeId::of::<E>(), self.next_reader_id);
-        let mut readers = self.res.fetch_mut::<HashMap<ReaderId, usize>>(0);
+        let reader_id = ReaderId::new(TypeId::of::<E>(), self.next_reader_id);
         self.next_reader_id += 1;
-        readers.insert(reader_id, 0);
         reader_id
     }
 
@@ -76,81 +91,47 @@ impl EventHandler {
         if events.len() == 0 {
             return;
         }
-        let mut storage = self.res.fetch_mut::<VecStorage<E>>(0);
-        let type_id = TypeId::of::<E>();
-        let mut writers = self.res.fetch_mut::<HashMap<TypeId, usize>>(0);
-        let write_index = writers.get_mut(&type_id).expect("Writer data not found!");
-        let start_write_index = *write_index;
+        let mut storage = self.res.fetch_mut::<RingBufferStorage<E>>(0);
+        let mut write_index = storage.write_index;
         for e in events.drain(0..) {
-            if *write_index == storage.data.len() {
+            if write_index == storage.data.len() {
                 storage.data.push(e);
             } else {
-                storage.data[*write_index] = e;
+                storage.data[write_index] = e;
             }
-            *write_index += 1;
-            if *write_index >= MAX_EVENTS {
-                *write_index = 0;
-            }
-        }
-        for (reader_id, reader_index) in self.res.fetch_mut::<HashMap<ReaderId, usize>>(0).iter_mut() {
-            if reader_id.0 == type_id && start_write_index < *reader_index &&
-                (*write_index >= *reader_index || *write_index < start_write_index) {
-                *reader_index = *write_index;
+            write_index += 1;
+            if write_index >= storage.max_size {
+                write_index = 0;
             }
         }
+        storage.write_index = write_index;
     }
 
     pub fn write_single<E: Event>(&mut self, event: &E) {
-        let mut storage = self.res.fetch_mut::<VecStorage<E>>(0);
-        let type_id = TypeId::of::<E>();
-        let mut writers = self.res.fetch_mut::<HashMap<TypeId, usize>>(0);
-        let write_index = writers.get_mut(&type_id).expect("Writer data not found!");
-        let start_write_index = *write_index;
-        if *write_index == storage.data.len() {
+        let mut storage = self.res.fetch_mut::<RingBufferStorage<E>>(0);
+        let mut write_index = storage.write_index;
+        if write_index == storage.data.len() {
             storage.data.push(event.clone());
         } else {
-            storage.data[*write_index] = event.clone();
+            storage.data[write_index] = event.clone();
         }
-        *write_index += 1;
-        if *write_index >= MAX_EVENTS {
-            *write_index = 0;
+        write_index += 1;
+        if write_index >= storage.max_size {
+            write_index = 0;
         }
-        for (reader_id, reader_index) in self.res.fetch_mut::<HashMap<ReaderId, usize>>(0).iter_mut() {
-            if reader_id.0 == type_id && start_write_index < *reader_index &&
-                (*write_index >= *reader_index || *write_index < start_write_index) {
-                *reader_index = *write_index;
-            }
-        }
+        storage.write_index = write_index;
     }
 
-    pub fn clear<E : Event>(&mut self) {
-        let mut storage = self.res.fetch_mut::<VecStorage<E>>(0);
-        storage.data.clear();
-        let type_id = TypeId::of::<E>();
-        let mut writers = self.res.fetch_mut::<HashMap<TypeId, usize>>(0);
-        let write_index = writers.get_mut(&type_id).expect("Writer data not found!");
-        *write_index = 0;
-        for (reader_id, reader_index) in self.res.fetch_mut::<HashMap<ReaderId, usize>>(0).iter_mut() {
-            if reader_id.0 == type_id {
-                *reader_index = 0;
-            }
-        }
-    }
-
-    pub fn read<E : Event + std::fmt::Debug>(&mut self, reader_id : ReaderId) -> Vec<E> {
-        let storage = self.res.fetch_mut::<VecStorage<E>>(0);
-        let mut readers = self.res.fetch_mut::<HashMap<ReaderId, usize>>(0);
-        let read_index = readers.get_mut(&reader_id).expect("Reader data not found!");
-        let mut writers = self.res.fetch_mut::<HashMap<TypeId, usize>>(0);
-        let write_index = writers.get_mut(&TypeId::of::<E>()).expect("Writer data not found!");
-        let read_data = if write_index >= read_index {
-            storage.data.get(*read_index..*write_index).unwrap().to_vec()
+    pub fn read<E : Event + std::fmt::Debug>(&mut self, reader_id : &mut ReaderId) -> Vec<E> {
+        let storage = self.res.fetch::<RingBufferStorage<E>>(0);
+        let read_data = if storage.write_index >= reader_id.read_index {
+            storage.data.get(reader_id.read_index..storage.write_index).unwrap().to_vec()
         } else {
-            let mut d = storage.data.get(*read_index..MAX_EVENTS).unwrap().to_vec();
-            d.extend(storage.data.get(0..*write_index).unwrap().to_vec());
+            let mut d = storage.data.get(reader_id.read_index..storage.max_size).unwrap().to_vec();
+            d.extend(storage.data.get(0..storage.write_index).unwrap().to_vec());
             d
         };
-        *read_index = *write_index;
+        reader_id.read_index = storage.write_index;
         read_data
     }
 
